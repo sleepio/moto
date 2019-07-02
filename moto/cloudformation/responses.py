3516336c1d55269bf9da100059f21286ea9087fd
SeraBig <seraphi.sigma@bighealth.com>
2019-07-02 09:00:27 -0700

    Use safe loaders rather than loaders which pass all the tests. This may mean cloudformation functionality is broken within moto. However, the remove arbitrary code execution vulnerability is fixed with this change.

--- moto/cloudformation/responses.py
+++ moto/cloudformation/responses.py
@@ -336,7 +336,7 @@ class CloudFormationResponse(BaseResponse):
         except (ValueError, KeyError):
             pass
         try:
-            description = yaml.load(self._get_param('TemplateBody'), Loader=yaml.Loader)['Description']
+            description = yaml.load(self._get_param('TemplateBody'), Loader=yaml.SafeLoader)['Description']
         except (yaml.ParserError, KeyError):
             pass
         template = self.response_template(VALIDATE_STACK_RESPONSE_TEMPLATE)
